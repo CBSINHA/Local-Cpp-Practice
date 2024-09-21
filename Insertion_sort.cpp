f2fa44905765ccf9f0b5ced4eaf67d34d70005f8
Shashank Sinha <shashanksinha590@gmail.com>
2024-09-21 12:47:25 +0530

    Prep

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#include<iostream>//wrong
 using namespace std;
 void InsertionSort(int arr[],int n){
-    for(int i=0;i<=n-1;i++){
+    for(int i=0;i<n-1;i++){
         int j=i;
             while(j>0&&arr[j-1]>arr[j] ){
                 swap(arr[j-1],arr[j]);
@@ -13,7 +13,7 @@ void InsertionSort(int arr[],int n){
 }
 int main()
 {
-    int arr[]={1,3,7,2,9};
+    int arr[]={300,15,9,3,7,1,2,700};
     InsertionSort(arr,5);
  return 0;
 }
--- vector.cpp
+++ vector.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     vector<int> vec1={1,2,3,4,5};
+    vector<int>::iterator it;
+    it=vec1.begin()+2;
     vec1.push_back(10);//added an element to the last of the vector
     cout<<vec1[0]<<endl;
     cout<<vec1.at(1)<<endl;
@@ -12,7 +14,7 @@ int main()
     //cout<<vec1.at(6);//Exception
     vec1.at(0)=9;
     cout<<vec1.at(0)<<endl;
-    vec1.erase(2);
+    vec1.erase(it);
     for(int i:vec1)
     cout<<i<<" ";
  return 0;
